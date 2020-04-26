eec48e833eb73447eaf3cd040df0fbeb875b9833
Illia Levandovskyi <lis2889@gmail.com>
2020-04-26 16:31:39 +0300

    view components are added

---
+++ Globomantics/Components/CreditCardRatesViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Globomantics.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Globomantics.ViewModels;
+
+namespace Globomantics.Components
+{
+    public class CreditCardRatesViewComponent : ViewComponent
+    {
+        private readonly IRateService _rateService;
+
+        public CreditCardRatesViewComponent(IRateService rateService)
+        {
+            _rateService = rateService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string title, string subTitle)
+        {
+            var model = new CreditCardRatesVM()
+            {
+                Rates = _rateService.GetCreditCardRates(),
+                Title = title,
+                SubTitle = subTitle
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Globomantics/ViewModels/CreditCardRatesVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Globomantics.Core.Models;
+
+namespace Globomantics.ViewModels
+{
+    public class CreditCardRatesVM
+    {
+        public IReadOnlyCollection<Rate> Rates { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+    }
+}
