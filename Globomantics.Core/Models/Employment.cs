5bef03957f01526cdfe3e9f11ece243813b888c6
tillileva <tillileva@SUBDN457>
2020-04-28 10:43:00 +0200

    ivalidatable object implemented in employee entity

--- Globomantics.Core/Models/Employment.cs
+++ Globomantics.Core/Models/Employment.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Globomantics.Models
 {
-    public class Employment
+    public class Employment : IValidatableObject
     {
         public string CurrentType { get; set; }
         public string CurrentEmployerName { get; set; }
@@ -16,5 +14,36 @@ namespace Globomantics.Models
         public string PreviousEmployerName { get; set; }
         public double? PreviousAnnualIncome { get; set; }
         public string PreviousDuration { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (validationContext.ObjectInstance is Employment e)
+            {
+                if (e.CurrentDuration.Equals("1"))
+                {
+                    var vrs = new List<ValidationResult>();
+                    if (string.IsNullOrWhiteSpace(e.PreviousType))
+                        StoreError(vrs, $"{nameof(PreviousType)} value is missing");
+
+                    if (string.IsNullOrWhiteSpace(e.PreviousEmployerName))
+                        StoreError(vrs, $"{nameof(PreviousEmployerName)} value is missing");
+
+                    if (string.IsNullOrWhiteSpace(e.PreviousDuration))
+                        StoreError(vrs, $"{nameof(PreviousDuration)} value is missing");
+
+                    if (e.PreviousAnnualIncome is null)
+                        StoreError(vrs, $"{nameof(PreviousAnnualIncome)} value is missing");
+
+
+                    return vrs;
+                }
+            }
+            return new[] { ValidationResult.Success };
+        }
+
+        private static void StoreError(List<ValidationResult> vrs, string message)
+        {
+            vrs.Add(new ValidationResult(message));
+        }
     }
 }
